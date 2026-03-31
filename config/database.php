94e32f6f291d7bedf5588d3c63dfb69c894ad2b4
Midyass <sa8470883@gmail.com>
2026-03-31 19:41:25 +0100

    clean code

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
 
 
-define("DB_HOST", "127.0.0.1:3306");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "@Achrafs606@");
-define("DB_DATABASE", "php_db");
+
+define("DB_HOST", "host_db"); // the default 127.0.0.1:3306
+define("DB_USERNAME", "usdername_db");
+define("DB_PASSWORD", "password_db");
+define("DB_DATABASE", "name_db");
 
 $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
